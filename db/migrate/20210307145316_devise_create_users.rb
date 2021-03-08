296a02fd36e9bece9d96d7fbe0c99e9432c9fcae
Ytakagi66 <ptlv66httl@gmail.com>
2021-03-08 19:05:00 +0900

    ユーザー編集ページの作成

--- db/migrate/20210307145316_devise_create_users.rb
+++ db/migrate/20210307145316_devise_create_users.rb
@@ -4,13 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
+      t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
       t.integer :prefecture_id,     null: false
       t.date :birthday,             null: false
       t.text :self_info,            null: false
-      t.string :favorite,           null: false
+      t.string :favorite,           null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
