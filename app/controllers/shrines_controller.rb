bbfb24d830692f4b311ac035dd44467a0f0c2ed9
Ytakagi66 <ptlv66httl@gmail.com>
2021-03-13 00:13:37 +0900

    神社投稿ページの作成

--- app/controllers/shrines_controller.rb
+++ app/controllers/shrines_controller.rb
@@ -1,5 +1,19 @@
 class ShrinesController < ApplicationController
   def index
-    @shirines = Shrine.new
+    @shrines = Shrine.all.page(params[:page])
+  end
+
+  def show
+    @Shrine = Shrine.find(params[:id])
+  end
+
+  def new
+    @shrine = Shrine.new
+  end
+
+  private
+
+  def shrine_params
+    params.require(:shrine).permit(:name, :info, :benefits_id, :postal_code, :prefecture_id, :city, :address, :latitude, :longitude).merge(user_id: current_user.id)
   end
 end
--- app/models/shrine.rb
+++ app/models/shrine.rb
@@ -1,2 +1,15 @@
 class Shrine < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :info, length: { maximum: 1000 }
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :prefecture_id
+    validates :benefits_id
+  end
 end
