763468bd0e1d0d494cb81ad312e2af0976c3780e
Ytakagi66 <ptlv66httl@gmail.com>
2021-03-17 20:31:36 +0900

    画像投稿機能のプレビューの修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,6 +3,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     const ImageList = document.getElementById('image-list');
     const ImagePreview = document.getElementById('image_preview');
     ImagePreview.setAttribute('style','display:flex;');
+    const InputShrine = document.getElementById('input_shrine');
 
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
@@ -10,7 +11,11 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       imageElement.setAttribute('class', "image-element")
       let imageElementNum = document.querySelectorAll('.image-element').length
       if (imageElementNum == 4){
-        imageElement.setAttribute('display: none')
+        imageElement.setAttribute('style','display: none')
+      }
+      if (imageElementNum == 3){
+        const element = document.querySelector('i')
+        element.remove()
       }
 
       // 表示する画像を生成
@@ -28,10 +33,17 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
         ImageList.setAttribute('class', `image_num_${imageElementNum}`)
         ImagePreview.setAttribute('class', `preview_num_${imageElementNum}`)
 
+        // 削除ボタンを生成
+        const spanHTML = document.createElement('span')
+        spanHTML.setAttribute('id', `shrine_span_${imageElementNum}`)
+        spanHTML.setAttribute('class', 'span_delete')     
+
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       imageElement.appendChild(inputHTML);
+      imageElement.appendChild(spanHTML);
       ImagePreview.appendChild(imageElement);
+    
 
       inputHTML.addEventListener('change', (e) => {
         file = e.target.files[0];
