a8471606c158a137646893c24803245d18a394e4
Ytakagi66 <ptlv66httl@gmail.com>
2021-03-19 21:23:59 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :shrines, only: [:index, :new, :create]
+  resources :shrines, only: [:index, :new, :create, :show]
 end
