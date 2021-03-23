ebbc61b1c0b8dec79f05f72b748629f81c7f6548
Ytakagi66 <ptlv66httl@gmail.com>
2021-03-23 21:32:21 +0900

    センターピンを立てる

--- app/javascript/shrines/map.js
+++ app/javascript/shrines/map.js
@@ -1,11 +1,18 @@
 
   var map;
+  var marker;
   function initMap() {
     shrine = JSON.parse(document.getElementById('map').dataset.json);
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: shrine.latitude, lng: shrine.longitude},
-      zoom: 19
+      zoom: 19,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      navigationControl: false
     });
-  }
+    marker = new google.maps.Marker({ // マーカーの追加
+      position: {lat: shrine.latitude, lng: shrine.longitude}, // マーカーを立てる位置を指定
+      map: map // マーカーを立てる地図を指定
+  });
+}
 
 window.addEventListener('load', initMap);
