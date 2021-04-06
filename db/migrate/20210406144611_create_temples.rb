e3f86ab8560780f2d0ac7287761daa07792389d1
Ytakagi66 <ptlv66httl@gmail.com>
2021-04-06 23:48:55 +0900

    templeテーブルの作成

--- db/migrate/20210406144611_create_temples.rb
+++ db/migrate/20210406144611_create_temples.rb
@@ -1,7 +1,13 @@
 class CreateTemples < ActiveRecord::Migration[6.0]
   def change
     create_table :temples do |t|
-
+      t.string :name,           null: false, unique: true
+      t.string :info,           null: false
+      t.integer :benefits_id,   null: false
+      t.string :address,        null: false
+      t.float :latitude,        null: false, default: nil
+      t.float :longitude,       null: false, default: nil
+      t.references :user,       null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_101633) do
+ActiveRecord::Schema.define(version: 2021_04_06_144611) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -144,6 +144,19 @@ ActiveRecord::Schema.define(version: 2021_04_04_101633) do
     t.index ["user_id"], name: "index_shrines_on_user_id"
   end
 
+  create_table "temples", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "info", null: false
+    t.integer "benefits_id", null: false
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_temples_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -179,4 +192,5 @@ ActiveRecord::Schema.define(version: 2021_04_04_101633) do
   add_foreign_key "shrine_natures", "shrines"
   add_foreign_key "shrine_photos", "shrines"
   add_foreign_key "shrines", "users"
+  add_foreign_key "temples", "users"
 end
