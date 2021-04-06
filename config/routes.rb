ae29e8be28c1f6eedecad672d9f5944884efeefb
Ytakagi66 <ptlv66httl@gmail.com>
2021-04-06 23:45:24 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
     resources :natures, only: [:create, :show, :index]
     resources :festivals, only: [:create, :show, :index]
   end
+
+  resources :temples, only: [:index, :new, :create, :show] do
+    resources :comments, only: [:create, :show, :index]
+    resources :goshuins, only: [:create, :show, :index]
+    resources :builds, only: [:create, :show, :index]
+    resources :histories, only: [:create, :show, :index]
+    resources :natures, only: [:create, :show, :index]
+    resources :festivals, only: [:create, :show, :index]
+  end
 end
